0260e390b3a5ff87e317c3673b35a0990e192193
adolfo.ponce@vass.com.mx <berenice.miranda@vass.com.mx>
2022-08-31 17:28:32 -0500

    Using launch, async and runblocking compilers to run coroutines.

--- app/src/main/java/com/example/coroutines_example/MainActivity.kt
+++ app/src/main/java/com/example/coroutines_example/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.coroutines_example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "ActividadPrincipal"
@@ -15,24 +13,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "Funcion Main iniciada")
+        Log.d(TAG, "Funcion Main Iniciada")
 
-        GlobalScope.launch {
-            val respuesta  = getInternetInfo()
-            Log.d(TAG, respuesta)
+        runBlocking {
+            Log.d(TAG, "Iniciando corrutina con : ${Thread.currentThread().name} con runblocking")
+            delay(2000)
+            Log.d(TAG, "Finalizando corrutina con: ${Thread.currentThread().name} con runblocking")
         }
+        Log.d(TAG, "Funcion Main FInalizada")
 
-      /*  GlobalScope.launch {
-            Log.d(TAG, "Corriendo corrutinas en el subproceso: ${Thread.currentThread().name} ")
-            delay(1000) //Esta es una funcion suspendible que indica el tiempo de espera
-            Log.d(TAG, "Mundo! ")
+        
+       /* GlobalScope.async {
+            val suma1 = async {
+                suma (2, 5)
+            }
+
+            val suma2 = async {
+                suma (8, 2)
+            }
+
+            val total = suma (suma1.await(), suma2.await())
+            Log.d(TAG, "TOTAL: $total")
+
+        }*/
+
+       /* val job = GlobalScope.launch {
+            Log.d(TAG,"Corrutina Iniciada")
+            delay(5000)
+            Log.d(TAG, "Corrutina terminada")
         }
-        Log.d(TAG, "Corriendo codigo en el subproceso: ${Thread.currentThread().name} ")
-        Log.d(TAG, "Hola")*/
-    }
-
-    suspend fun getInternetInfo() :String {
-        delay(2000)
-        return "Respuesta desde internet"
+        Ejectutar.setOnClickListener {
+            job.cancel()
+            Log.d(TAG, "Corrutina Cancelada")
+        }*/
     }
 }
+ fun suma (num1 : Int, num2 : Int) = num1 + num2
